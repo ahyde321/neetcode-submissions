7ca65111f9f21eefbe2f1a3d785227be8ab55a2a
ahyde321 <119438549+ahyde321@users.noreply.github.com>
2026-05-03 10:35:31 +0100

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.py
@@ -0,0 +1,50 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        s1_count, s2_count = [0] * 26, [0] * 26
+
+        # Populate arrays with len(s1) characters
+        for i in range(len(s1)):
+            s1_count[ord(s1[i]) - ord('a')] += 1
+            s2_count[ord(s2[i]) - ord('a')] += 1
+        
+        # Calculate matches between base sliding window and s2
+        matches = 0
+        for i in range(len(s1_count)):
+            if s1_count[i] == s2_count[i]:
+                matches += 1
+        
+        # Sliding the window incrementally by 1, calculating matches each iteration
+        l = 0    
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+            
+            index = ord(s2[r]) - ord('a')
+            s2_count[index] += 1
+
+            if s1_count[index] == s2_count[index]:
+                matches += 1
+            elif s1_count[index] + 1 == s2_count[index]:
+                matches -= 1
+            
+            index = ord(s2[l]) - ord('a')
+            s2_count[index] -= 1
+
+            if s1_count[index] == s2_count[index]:
+                matches += 1
+            elif s1_count[index] == s2_count[index] + 1:
+                matches -= 1
+            
+            l += 1
+        
+        return matches == 26
+        
+
+
+
+        
+
+
